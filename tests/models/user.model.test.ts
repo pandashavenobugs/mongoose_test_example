b6a5ee8449edaab7719288bbca21ca985ab4f44a
cengiz berat <cedinckan@gmail.com>
2022-03-24 12:03:34 +0300

    arraySubDocument update test finished

--- tests/models/user.model.test.ts
+++ tests/models/user.model.test.ts
@@ -5,6 +5,7 @@ import UserModel, {
   UserInput,
 } from "../../src/models/User.model";
 import PostModel, { PostInput } from "../../src/models/Post.model";
+
 //https://stackoverflow.com/questions/26156687/mongoose-find-update-subdocument
 
 describe("connecting to db and testting the UserModel crud", () => {
@@ -43,8 +44,9 @@ describe("connecting to db and testting the UserModel crud", () => {
     expect(childPost).toBeDefined();
     // const newPostC = PostModel.create({ ...postInput });
   });
-
-  it("nested PostSchema update test", async () => {
+  // we have 2 ways to update the nestedDocuments
+  // 1st way
+  it("nested PostSchema update test in 1st way", async () => {
     const userInput: UserInput = {
       name: "userTest",
     };
@@ -54,16 +56,61 @@ describe("connecting to db and testting the UserModel crud", () => {
     };
     user!.posts!.push({ ...postInput });
     const newUSer = await user.save();
-    const post = newUSer!.posts![0];
 
-    const childPost = newUSer!.posts!.id(post._id);
-    // childPost!.title = "deneme";
-    childPost!.set({ title: "test" });
-    const index = newUSer!.posts!.indexOf(childPost!);
-    console.log(index);
-    await newUSer!.save();
-    console.log(newUSer!);
-    const fetchedUSer = await UserModel.findOne({ _id: newUSer._id });
-    console.log(fetchedUSer!);
+    const fetchedNewUser = await UserModel.findOne({ _id: newUSer._id });
+    const postAsNestedDocument = fetchedNewUser!.posts![0];
+    expect(postAsNestedDocument).toBeDefined();
+    expect(postAsNestedDocument.title).toEqual(postInput.title);
+
+    //Update Side
+    //fetch the post from posts
+    const postFetchedFromPosts = fetchedNewUser!.posts!.id(
+      postAsNestedDocument!._id
+    );
+    expect(postFetchedFromPosts).toBeDefined();
+
+    postFetchedFromPosts!.set({ title: "postTitle changed in 1st way" });
+    // we can update the data  also like that
+    // fetchedNewUser!
+    //   .posts!.id(postAsNestedDocument!._id)
+    //   .set({ title: "postTitle changed in 1st way" });
+    await fetchedNewUser!.save();
+
+    const updatedUser = await UserModel.findOne({ _id: fetchedNewUser!._id });
+    const updatedNestedPost = updatedUser!.posts!.id(postFetchedFromPosts!._id);
+    expect(updatedNestedPost!.title).toEqual("postTitle changed in 1st way");
+  });
+  it("nested PostSchema update test in 2nd way", async () => {
+    const userInput: UserInput = {
+      name: "userTest",
+    };
+    const user = new UserModel({ ...userInput });
+    const postInput: PostInput = {
+      title: "postTest",
+    };
+    user!.posts!.push({ ...postInput });
+    const newUSer = await user.save();
+
+    const fetchedNewUser = await UserModel.findOne({ _id: newUSer._id });
+    const postAsNestedDocument = fetchedNewUser!.posts![0];
+    expect(postAsNestedDocument).toBeDefined();
+    expect(postAsNestedDocument.title).toEqual(postInput.title);
+
+    //Update Side
+    const indexNumber = fetchedNewUser!.posts!.findIndex(
+      (post) => `${post._id}` === `${postAsNestedDocument!._id}`
+    );
+    const post = fetchedNewUser!.posts![indexNumber];
+    post!.title = "postTitle changed in 2nd way";
+    fetchedNewUser!.posts!.set(indexNumber, post);
+    // we can update the data  also like that
+    // fetchedNewUser!.posts!.set(indexNumber, {
+    //   title: "postTitle changed in 2nd way",
+    // } as any);
+    await fetchedNewUser!.save();
+    const updatedUser = await UserModel.findOne({ _id: fetchedNewUser!._id });
+    const updatedNestedPost = updatedUser!.posts!.id(postAsNestedDocument!._id);
+    // console.log(updatedNestedPost);
+    expect(updatedNestedPost!.title).toEqual("postTitle changed in 2nd way");
   });
 });
